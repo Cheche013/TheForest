7fb4c95b414aa829d6543ecf8dbfe59f19b9621f
Cheche013 <cheyenne.lefranc013@gmail.com>
2025-10-28 17:45:11 +0100

    maybe 4/4 bug resolt ?

--- config/application.rb
+++ config/application.rb
@@ -1,23 +1,19 @@
+# config/application.rb
 require_relative "boot"
-
 require "rails/all"
-
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module TheForest
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
 
-    # Please, add to the `ignore` list any other `lib` subdirectories that do
-    # not contain `.rb` files, or that should not be reloaded or eager loaded.
-    # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
+
+    # Chemins d'assets (propshaft)
     config.assets.paths << Rails.root.join("app", "assets", "images")
-    config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif]
     config.assets.paths << Rails.root.join("app", "assets", "audios")
 
+    # Types précompilés
+    config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif *.mp3 *.wav]
   end
 end
